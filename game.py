b904c8cc237e5ca1ae813aa7440fa80f2cc7245b
gracekumagai <g.kumagai@mail.utoronto.ca>
2017-03-27 02:46:13 -0400

    Finished start of Card and Hand Classes; Initalized cards and case file for gameplay

--- game.py
+++ game.py
@@ -1,18 +1,80 @@
 import time
 import functools
+import numpy as np
 
 class Game(object):
+	'''
+	self.rooms - list of room card variables (except for room card in case file)
+	self.weapons - list of weapon card variables (except for weapon card in case file)
+	self.suspects - list of suspect card variables (except for suspect card in case file)
+
+	self.caseFileRoom - Room card variable in the case file
+	self.caseFileSuspect - Suspect card variable in the case file
+	self.caseFileWeapon - Weapon card variable in the case file
+	'''
 
 	def __init__(self):
 		'''
-		- Create all cards
-		- Assign case file (randomly)
+		- [DONE] Create all cards
+		- [DONE] Assign case file (randomly)
 		- Initialize agents
-		- assign cards to agents (Randomly)
+		- assign cards to agents (Randomly)			
 		- Choose a player to go first
 		'''
-		pass
+		self._init_cards()
+		self._make_case_file()
+
+
+	def _init_cards(self):
+		'''
+		Initialize all 21 cards
+		9 rooms, 6 weapons, 6 suspects
+		Return lists of each type
+		'''
+		#Init Rooms
+		r1 = Card('Room', 'Conservatory')
+		r2 = Card('Room', 'Hall')
+		r3 = Card('Room', 'Lounge')
+		r4 = Card('Room', 'Dining Room')
+		r5 = Card('Room', 'Kitchen')
+		r6 = Card('Room', 'Ballroom')
+		r7 = Card('Room', 'Billiard Room')
+		r8 = Card('Room', 'Library')
+		r9 = Card('Room', 'Study')
+		self.rooms = [r1, r2, r3, r4, r5, r6, r7, r8, r9]
+		np.random.shuffle(self.rooms)
+
+		#Init weapons
+		w1 = Card('Weapon', 'Candlestick')
+		w2 = Card('Weapon', 'Revolver')
+		w3 = Card('Weapon', 'Wrench')
+		w4 = Card('Weapon', 'Rope')
+		w5 = Card('Weapon', 'Lead Pipe')
+		w6 = Card('Weapon', 'Knife')
+		self.weapons = [w1, w2, w3, w4, w5, w6]
+		np.random.shuffle(self.weapons)
+
+		#Init suspects
+		s1 = Card('Suspect', 'Miss Scarlett')
+		s2 = Card('Suspect', 'Mrs White')
+		s3 = Card('Suspect', 'Mr Green')
+		s4 = Card('Suspect', 'Mrs Peacock')
+		s5 = Card('Suspect', 'Colonel Mustard')
+		s6 = Card('Suspect', 'Professor Plum')
+		self.suspects = [s1, s2, s3, s4, s5, s6]
+		np.random.shuffle(self.suspects)
 	
+
+	def _make_case_file(self):
+		self.caseFileRoom = self.rooms[0]
+		self.caseFileWeapon = self.weapons[0]
+		self.caseFileSuspect = self.suspects[0]
+
+		self.rooms.pop(0)
+		self.weapons.pop(0)
+		self.suspects.pop(0)
+
+
 	def start(self):
 		'''
 		- keeping track of who's turn is it
@@ -38,23 +100,55 @@ class Game(object):
 
 
 class Card(object):
-	pass
 	'''
-	type
-	name
-	?owner
+	VARIABLE CLASS:
+	type = <Room, Suspect, Weapon>
+	name = <Miss Scarlett, Lead Pipe, Study .. >
+	owner - player that holds the card
+	dom - dom is list of names card can take on or 
+		- only contains name of card
+	curdom - current domain
+	assignedType
+	assignedName
 	'''
-	def __init__(self, type, name):
+	def __init__(self, type='', name='', domain=[]):
 		'''
-		init it with the things
+		Initialize a card with a type = <Room, Suspect, Weapon>
+		and a name = <Miss Scarlett, Lead Pipe, Lounge, ...>
 		'''
-		pass
+		self.type = type
+		self.name = name
+		self.player = None
+
+		self.dom = list(domain)
+		self.curdom = [True] * len(domain)
+		self.assignedType = None
+		self.assignedName = None
 
 	def assign(self, player):
 		'''
 		self explanatory
 		'''
-		pass
+		self.player = player
+
+	def getType(self):
+		return (self.type)
+	
+	def getName(self):
+		return (self.name)
+	
+	def getPlayer(self):
+		return (self.player)
+
+	def getDomainSize(self):
+		return (len(self.dom))
+
+	def getDom(self):
+		return (list(self.dom))
+
+	def print_all(self):
+		'''Also print the variable domain and current domain'''
+		print("Type, Name--\"{}{}\": Dom = {}, CurDom = {}".format(self.type, self.name, self.dom, self.curdom))
 
 
 class Hand(object):
@@ -65,13 +159,29 @@ class Hand(object):
 		'''
 		Initialize 6 empty card objects into hand
 		'''
-		pass
+		self.cards = [Card()]*6
 
 	def add_card(self, card):
 		'''
 		Initialize an empty card as card if empty cards exist
+
+		NOTE: MIGHT NEED TO MAKE THIS MORE SPECIFIC TO ENSURE TO TARGETS 
+		THE RIGHT CARD, for now:
 		'''
-		pass
+		for i, c in enumerate(self.cards):
+			if c.getName() == None:
+				c = card
+				self.cards[i] = card
+				break
+
+	def isInHand(self, name):
+		'''
+		Returns true if a card in the hand is assigned name
+		'''
+		for c in self.cards:
+			if c.name == name:
+				return True
+		return False
 
 
 class Agent(object):
@@ -104,5 +214,5 @@ class Agent(object):
 		pass
 
 
-
+if __name__ == '__main__':
 
